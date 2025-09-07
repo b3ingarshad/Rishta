1a7316ff02ba3f55c90c1cfeee29a6fef1af0a77
Arshad <arshadmiya34@gmail.com>
2025-09-07 17:43:31 +0530

    10

--- server/server.js
+++ server/server.js
@@ -1,29 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
-
 require('dotenv').config();
 
 const app = express();
 
 // Middleware 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.use(cors({
-  origin: [
-    'https://rishta-b7de.vercel.app', // Your NEW live Vercel frontend
-    'https://rishta-ochre.vercel.app', // Your old URL (keep it if you still use it)
-    'http://localhost:3000' // Your local frontend for development
-  ]
-}));
+app.use(cors({ origin: ['https://rishta-ochre.vercel.app', 'http://localhost:3000'],
+  methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH, PROPFIND',
+  credentials: true
+ }));
 app.use(express.json());
-app.use((req, res, next) => {
-  if (req.headers.host.slice(0, 4) !== 'www.') {
-    res.redirect(301, 'https://www.' + req.headers.host + req.originalUrl);
-  }
-  next();
-});
+
 // Database connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -32,14 +20,13 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.log(err));
 
+app.get('/',(req,res)=>{
+ res.send("API is working............")
+})
 const userRoutes = require('./routes/users');
 const authRoutes = require("./routes/auth");
 app.use('/api/users', userRoutes);
 app.use("/auth", authRoutes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 
