c9005a7f1d2d932fa10e20a76643572b70e8fb6c
Arshad <arshadmiya34@gmail.com>
2025-09-07 17:20:08 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware 
-app.use(cors({ origin: ['https://rishta-ochre.vercel.app/', 'http://localhost:3000'] }));
+app.use(cors({ origin: ['https://rishta-ochre.vercel.app', 'http://localhost:3000'] }));
 app.use(express.json());
 
 // Database connection
