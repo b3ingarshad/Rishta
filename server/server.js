9888ee10eadba20ab025550debc26cd10b35c809
Arshad <arshadmiya34@gmail.com>
2025-09-07 17:32:07 +0530

    Changes in backend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
 
 // Middleware 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(cors({ origin: ['https://rishta-ochre.vercel.app', 'http://localhost:3000'] }));
 app.use(express.json());
 
@@ -21,6 +25,10 @@ const userRoutes = require('./routes/users');
 const authRoutes = require("./routes/auth");
 app.use('/api/users', userRoutes);
 app.use("/auth", authRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
