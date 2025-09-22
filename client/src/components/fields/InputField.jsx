bee71d606724837b65ae4fd6b975dc440a5f9fba
cyberstarsystem <cyberstarsystem@gmail.com>
2025-09-22 13:24:10 +0530

    SignUp Changes

--- client/src/components/fields/InputField.jsx
+++ client/src/components/fields/InputField.jsx
@@ -10,6 +10,8 @@ function InputField(props) {
     disabled,
     value,        // new: controlled value
     onChange,
+    style,
+    className
     
   } = props;
 
@@ -27,18 +29,22 @@ function InputField(props) {
         disabled={disabled}
         type={type}
         id={id}
+        style={style}
         placeholder={placeholder}
        value={type === "file" ? undefined : value}      // controlled value here
         onChange={onChange} // change handler here
-        className={`mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none ${
-          disabled === true
+         className={`mt-2 flex h-12 w-full items-center justify-center rounded-xl 
+          border bg-white/0 p-3 text-sm outline-none
+          ${disabled === true
             ? "!border-none !bg-gray-100 dark:!bg-white/5 dark:placeholder:!text-[rgba(255,255,255,0.15)]"
             : state === "error"
             ? "border-red-500 text-red-500 placeholder:text-red-500 dark:!border-red-400 dark:!text-red-400 dark:placeholder:!text-red-400"
             : state === "success"
             ? "border-green-500 text-green-500 placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400"
             : "border-gray-200 dark:!border-white/10 dark:text-white"
-        }`}
+          }
+          ${className || ""}   // 👉 user class override
+        `}
       />
     </div>
   );
